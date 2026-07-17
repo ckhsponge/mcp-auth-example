59c68ffc539f68bbb0a5cf08a6834b1a330f6443
Chris Hobbs <purposemc@gmail.com>
2026-07-17 15:22:19 -0700

    authentication comment

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -16,6 +16,10 @@ class Tool
   end
 
   def self.handle_user(user_id, user_oauth_identifier, tool_target_identifier, event)
+    # use user_id and oauth identifier to get authenticated user
+    #     user_oauth = UserOauth.find_by(user_id: user_id, identifier: user_oauth_identifier)
+    #     raise ArgumentError, "no user_oauth found for user_id #{user_id} and identifier #{user_oauth_identifier}" unless user_oauth
+
     mcp_text_result("The current date and time is #{Time.now.utc.strftime('%Y-%m-%d %H:%M:%S UTC')}.")
   end
 end
